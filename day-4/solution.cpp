89f385c0698f1c02b0462e1b01d0ea0e90578344
Daniel <daniel.1302@gmail.com>
2018-12-22 21:53:37 +0100

    Day 4

--- day-4/solution.cpp
+++ day-4/solution.cpp
@@ -4,12 +4,15 @@
 #include<vector>
 #include<regex>
 #include<unordered_map>
+#include<algorithm>
+
 
 struct Guardian
 {
     int id;
     std::array<int, 60> minutes{};
     int asleepTime = 0;
+    std::pair<int, int> mostCommonMinute;
 };
 
 
@@ -65,7 +68,7 @@ std::vector<GuardianEvent> parseInput(std::vector<std::string>& input) {
             event.type = GuardianEvent::TYPE::AWAKE;
             event.guardianId = 0;
         } else if (std::regex_match(item, match_results, reAsleep) && match_results.size() > 0) {
-            event.type = GuardianEvent::TYPE::ASLEEP; 
+            event.type = GuardianEvent::TYPE::ASLEEP;
             event.guardianId = 0;
         } else {
             continue;
@@ -82,25 +85,77 @@ std::vector<GuardianEvent> parseInput(std::vector<std::string>& input) {
     return output;
 }
 
-int step1(std::vector<GuardianEvent> events) 
+std::pair<int, int> solution(std::vector<GuardianEvent>& events)
 {
     std::unordered_map<int, Guardian> guardians{};
 
     int currentGuardianId = 0;
-    GuardianEvent& currentEvent;
+    GuardianEvent& currentEvent = events[0];
 
-    auto registerAsleep = [&](GuardianEvent& e1, GuardianEvent& e2) {
-        
+    auto registerAsleep = [&](Guardian& guardian, GuardianEvent& e1, GuardianEvent& e2) {
+        // std::cout<<"("<<guardianId<<")"<<e1.time.hr<<":"<<e1.time.mi<<" - "<<e2.time.hr<<":"<<e2.time.mi<<std::endl;
+
+        int start  = std::max({0, e1.time.mi});
+        int finish = std::min({59, e2.time.mi});
+
+        guardian.asleepTime += (finish + 1 - start);
+
+        for (int t=start; t<finish; t++) {
+            guardian.minutes[t]++;
+        }
     };
 
-    auto createGuardian = [&](int guardianId) {
-        if (guardians.find(guardianId) != guardians.end()) {
-            return;
+    auto getGuardian = [&](int guardianId) -> Guardian& {
+        auto it = guardians.find(guardianId);
+
+        if (it != guardians.end()) {
+            return (it->second);
         }
 
         guardians[guardianId] = Guardian();
-    }
-    
+        guardians[guardianId].id = guardianId;
+
+        return guardians[guardianId];
+    };
+
+
+
+
+    auto findTheMostCommonMinute = [](Guardian& guardian) -> std::pair<int, int> {
+
+        auto maxElement = std::max_element(guardian.minutes.begin(), guardian.minutes.end());
+
+        return std::make_pair(
+            maxElement - guardian.minutes.begin(),
+            *maxElement
+        );
+    };
+
+    auto findTheMostRegularPerson = [](std::unordered_map<int, Guardian>& guardians) -> Guardian {
+        Guardian& _guardian = guardians[0];
+
+        for (auto& g : guardians) {
+            if (g.second.mostCommonMinute.second > _guardian.mostCommonMinute.second) {
+                _guardian = g.second;
+            }
+        }
+
+        return _guardian;
+    };
+
+    auto findLeziesGuardian = [](std::unordered_map<int, Guardian>& guardians) -> Guardian {
+
+        Guardian& guardian = guardians[0];
+
+        for (auto& g : guardians) {
+            if (g.second.asleepTime > guardian.asleepTime) {
+                guardian = g.second;
+            }
+        }
+
+        return guardian;
+    };
+
 
     for (auto& e : events) {
         if (currentGuardianId == 0) {
@@ -109,20 +164,40 @@ int step1(std::vector<GuardianEvent> events)
             continue;
         }
 
-        createGuardian(currentGuardianId);
-
-        if (e.type == GuardianEvent::TYPE::AWAKE && currentState == GuardianEvent::TYPE::ASLEEP) {
-            
+        if (e.type == GuardianEvent::TYPE::AWAKE && currentEvent.type == GuardianEvent::TYPE::ASLEEP) {
+            registerAsleep(getGuardian(currentGuardianId), currentEvent, e);
         }
 
+        currentEvent = e;
+
+        if (e.type == GuardianEvent::TYPE::SHIFT) {
+            currentGuardianId = e.guardianId;
+        }
     }
+
+    for (auto& g : guardians) {
+        g.second.mostCommonMinute = findTheMostCommonMinute(g.second);
+    }
+
+
+    Guardian theLeziestGuardian = findLeziesGuardian(guardians);
+    Guardian theMostRegularGuardian = findTheMostRegularPerson(guardians);
+
+
+    return std::make_pair(
+        theLeziestGuardian.mostCommonMinute.first*theLeziestGuardian.id,
+        theMostRegularGuardian.mostCommonMinute.first*theMostRegularGuardian.id
+    );
 }
 
 int main()
 {
     std::vector<std::string> inputLines = readInput("input");
     std::vector<GuardianEvent> events = parseInput(inputLines);
-    std::cout<<step1(events);
+    auto results = solution(events);
+
+    std::cout<<results.first<<std::endl<<results.second<<std::endl;
+
 
     return 0;
 }
