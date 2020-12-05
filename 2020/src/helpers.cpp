bbf8bf7c2468d8fadf8a979bfeb1d9c80fe4b473
Daniel Hornik <daniel.1302@gmail.com>
2020-12-05 22:27:45 +0100

    Add tokenize function

--- 2020/src/helpers.cpp
+++ 2020/src/helpers.cpp
@@ -1,8 +1,15 @@
-#include <vector>
+#pragma once
+
+
 #include <string>
-#include <sstream>
+#include <regex>
+#include <algorithm>
+#include <vector>
 #include <fstream>
+#include <sstream>
 #include <numeric>
+#include <iostream>
+#include <iterator>
 
 std::vector<int> loadIntegers(const std::string& filename) {
     std::ifstream input{filename};
@@ -17,7 +24,27 @@ std::vector<int> loadIntegers(const std::string& filename) {
 
 template <typename T>
 std::string join(const T& vec, std::string delim) {
-    return std::accumulate(vec.begin(), vec.end(), std::string{}, [&delim](std::string& init, const typename T::value_type& item) -> std::string {
-        return std::move(init) + ((init.length() > 0) ? delim : "") +  (std::stringstream{} << item).str();
-    });
+    return std::accumulate(vec.begin(), vec.end(), std::string{}, 
+        [&delim](std::string init, const typename T::value_type& item) -> std::string {
+            return std::move(init) 
+                + ((init.length() > 0) ? delim : "") 
+                + (std::stringstream{item}).str();
+        }
+    );
+}
+
+
+
+
+std::vector<std::string> tokenize(const std::string& input, const std::string& token) {
+    std::vector<std::string> res{};
+
+    std::regex tokenRegex{token};
+    std::copy(
+        std::sregex_token_iterator{input.begin(), input.end(), tokenRegex, -1},
+        std::sregex_token_iterator{},
+        std::back_inserter(res)       
+    );
+
+    return res;
 }
