535f4227efea54528cbb8cb47174ce549306c7d1
daniel1302 <daniel.1302@gmail.com>
2022-12-17 23:05:40 +0100

    feat: day9 part2

--- 2022/day-09/src/lib.rs
+++ 2022/day-09/src/lib.rs
@@ -8,28 +8,48 @@ enum Move {
     Right,
 }
 
-fn compute_tail_position(m: &Move, current_tail_pos: (u32, u32), new_head_pos: (u32, u32)) -> (u32, u32) {
-    if new_head_pos == current_tail_pos {
-        return current_tail_pos;
+fn compute_tail_position(current_tail_pos: (u32, u32), new_head_pos: (u32, u32)) -> (u32, u32) {
+    let is_connected = |t1: (u32, u32), t2: (u32, u32)| {
+        let distance = i32::abs(t1.0 as i32 - t2.0 as i32) + i32::abs(t1.1 as i32 - t2.1 as i32);
+        t1 == t2 || distance == 1 || (
+            current_tail_pos.0 != new_head_pos.0 
+            && current_tail_pos.1 != new_head_pos.1 
+            && distance == 2 
+        )
+    };
+
+    if is_connected(new_head_pos, current_tail_pos) {
+        return current_tail_pos
     }
 
-    if current_tail_pos.0 != new_head_pos.0 
-        && current_tail_pos.1 != new_head_pos.1 
-        && i32::abs(new_head_pos.0 as i32 -current_tail_pos.0 as i32) + i32::abs(new_head_pos.1 as i32 -current_tail_pos.1 as i32) == 2 
-    {
-        return current_tail_pos;
+    if new_head_pos.0 == current_tail_pos.0 {
+        if new_head_pos.1 < current_tail_pos.1 {
+            return (current_tail_pos.0, current_tail_pos.1-1); // left
+        } else {
+            return (current_tail_pos.0, current_tail_pos.1+1); // left
+        }
     }
 
-    if i32::abs(new_head_pos.0 as i32 -current_tail_pos.0 as i32) + i32::abs(new_head_pos.1 as i32 -current_tail_pos.1 as i32) == 1 {
-        return current_tail_pos;
+    if new_head_pos.1 == current_tail_pos.1 {
+        if new_head_pos.0 < current_tail_pos.0 {
+            return (current_tail_pos.0-1, current_tail_pos.1); // up
+        } else {
+            return (current_tail_pos.0+1, current_tail_pos.1); // down
+        }
     }
 
-    return match m {
-        Move::Up => (new_head_pos.0, new_head_pos.1+1),
-        Move::Down => (new_head_pos.0, new_head_pos.1-1),
-        Move::Left => (new_head_pos.0+1, new_head_pos.1),
-        Move::Right => (new_head_pos.0-1, new_head_pos.1),
+    // diagonal moves
+    if is_connected(new_head_pos, (current_tail_pos.0-1, current_tail_pos.1-1)) {
+        return (current_tail_pos.0-1, current_tail_pos.1-1);
+    } else if is_connected(new_head_pos, (current_tail_pos.0-1, current_tail_pos.1+1)) {
+        return (current_tail_pos.0-1, current_tail_pos.1+1);
+    } else if is_connected(new_head_pos, (current_tail_pos.0+1, current_tail_pos.1-1)) {
+        return (current_tail_pos.0+1, current_tail_pos.1-1);
+    } else if is_connected(new_head_pos, (current_tail_pos.0+1, current_tail_pos.1+1)) {
+        return (current_tail_pos.0+1, current_tail_pos.1+1);
     }
+
+    panic!("Unexpected move");
 }
 
 pub fn process_part1(input: &str) -> String {
@@ -50,19 +70,20 @@ pub fn process_part1(input: &str) -> String {
     .flatten()
     .for_each(|m| {
         match m {
-            Move::Up => current_head_position.1 += 1,
-            Move::Down => current_head_position.1 -= 1,
+            Move::Up => current_head_position.1 -= 1,
+            Move::Down => current_head_position.1 += 1,
             Move::Left => current_head_position.0 -= 1,
             Move::Right => current_head_position.0 += 1,
         }
         
-        current_tail_position = compute_tail_position(&m, current_tail_position, current_head_position);
+        current_tail_position = compute_tail_position(current_tail_position, current_head_position);
         tail_positions.insert(current_tail_position.clone());
     });
     
     tail_positions.len().to_string()
 }
 
+#[allow(unused)]
 fn print(rope: &Vec<(u32, u32)>, width: usize, height: usize) {
     let mut buffer = vec!['.'; width*height];
     
@@ -70,10 +91,15 @@ fn print(rope: &Vec<(u32, u32)>, width: usize, height: usize) {
         return (width*knot.1 as usize + knot.0 as usize);
     };
 
-    buffer[calc_pos(&rope[0])] = 'H';
 
-    for (idx, knot_pos) in rope.iter().enumerate().skip(1) {
-        buffer[calc_pos(knot_pos)] = (48 + idx as u8) as char;
+    for (idx, knot_pos) in rope.iter().enumerate().rev() {
+        if idx == 0 {
+            buffer[calc_pos(knot_pos)] = 'H';
+        } else if idx == 9 {
+            buffer[calc_pos(knot_pos)] = 'T';
+        } else {
+            buffer[calc_pos(knot_pos)] = (48 + idx as u8) as char;
+        }
     }
 
     println!("{:?}", rope);
@@ -84,18 +110,17 @@ fn print(rope: &Vec<(u32, u32)>, width: usize, height: usize) {
             println!("");
         }
     }
-    println!("\n\n\n");
 
+    println!("\n\n\n");
 }
 
 
 pub fn process_part2(input: &str) -> String {
     let mut tail_positions: HashSet<(u32, u32)> = HashSet::new();
 
-    let mut rope_position: Vec<(u32, u32)> = vec![(15u32, 15u32); 10];
+    let mut rope_position: Vec<(u32, u32)> = vec![(1000u32, 1000u32); 10];
     const HEAD_ID: usize = 0;
     const TAIL_ID: usize = 9;
-
 
     input.lines().map(|line| {
         let move_instructions = line.split(" ").collect::<Vec<&str>>();
@@ -118,13 +143,11 @@ pub fn process_part2(input: &str) -> String {
         }
 
         rope_position[HEAD_ID] = head_position;
-
         for (knot_idx, _knot_position) in rope_position.clone().iter().enumerate().skip(1) {
-            rope_position[knot_idx] = compute_tail_position(&m, rope_position[knot_idx], rope_position[knot_idx-1]);
+            rope_position[knot_idx] = compute_tail_position(rope_position[knot_idx], rope_position[knot_idx-1]);
         }
         
         // print(&rope_position, 33, 33);
-        println!("{:?}, {}", rope_position[TAIL_ID], tail_positions.len());
         tail_positions.insert(rope_position[TAIL_ID].clone());
     });
     
@@ -135,11 +158,7 @@ pub fn process_part2(input: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
-    #[test]
-    #[ignore]
-    fn part1() {
-        const INPUT: &str = "R 4
+    const INPUT: &str = "R 4
 U 4
 L 3
 D 1
@@ -148,12 +167,14 @@ D 1
 L 5
 R 2";
 
+    #[test]
+    fn part1() {
         assert_eq!(process_part1(INPUT), "13");
     }
 
     #[test]
     fn part2() {
-        const INPUT: &str = "R 5
+        const INPUT2: &str = "R 5
 U 8
 L 8
 D 3
@@ -162,7 +183,7 @@ D 10
 L 25
 U 20";
 
-        
-        assert_eq!(process_part2(INPUT), "36");
+        // assert_eq!(process_part2(INPUT), "1");
+       assert_eq!(process_part2(INPUT2), "36");
     }
 }
