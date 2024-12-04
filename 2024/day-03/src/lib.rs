70a71168df1f2605bb2b0960be169c484dae6818
daniel1302 <daniel.1302@gmail.com>
2024-12-04 22:15:38 +0100

    feat: day3

--- 2024/day-03/src/lib.rs
+++ 2024/day-03/src/lib.rs
@@ -1,84 +1,77 @@
-use nom::{
-    bytes::complete::tag, number::complete::i32, sequence::{delimited, preceded}
-};
+use regex::Regex;
 
-// #::IResult::complete::tag[derive(Debug)]
-// struct Op {
-//     p1: i32,
-//     p2: i32
-// }
-
-enum Token {
+#[derive(Debug)]
+enum Op {
+    Unknown,
     DoNot,
     Do,
     Mul(i32, i32),
 }
 
-// fn parse_mul(input: &str) -> IResult<&str, Token::Mul> {
-//     let res = preceded(
-//         tag("mul"),
-//         delimited(
-//             tag("("), 
-//             delimited(i32(nom::number::Endianness::Native), ",", i32(nom::number::Endianness::Native)),
-//             tag(")")
-//         )
-//     )(input)?;
+fn parse_input(input: &str) -> Vec<Op> {
+    let re = Regex::new(
+        r"(?<mnemonic>mul\((?<p1>\d+),(?<p2>\d+)\)|do\(\)|don't\(\))",
+    ).unwrap();
 
-//     let 
+    re.captures_iter(input).map(|caps| {
+        let mnemonic = caps.name("mnemonic").unwrap().as_str();
+        if mnemonic.starts_with("don't") {
+            Op::DoNot
+        } else if mnemonic.starts_with("do") {
+            Op::Do
+        } else if mnemonic.starts_with("mul") {
+            caps.name("p1").unwrap().as_str();
+            caps.name("p2").unwrap().as_str();
 
-//     Ok(())
-// }
-
-fn process_input(input: &str, with_disabled_memory: bool) -> Vec<Op> {
-
-
-    // re
-    //     .captures_iter(input)
-    //     .map(|caps| {
-    //         Op{
-    //             p1: caps.name("p1").unwrap().as_str().parse::<i32>().unwrap(),
-    //             p2: caps.name("p2").unwrap().as_str().parse::<i32>().unwrap(),
-    //         }
-    //     })
-    //     .collect()
+            Op::Mul(
+                caps.name("p1").unwrap().as_str().parse::<i32>().unwrap(),
+                caps.name("p2").unwrap().as_str().parse::<i32>().unwrap(),
+            )
+        } else {
+            Op::Unknown
+        }
+    })
+    .collect()
 }
 
-fn remove_disabled_memory(input: &str) -> String {
-    let re = Regex::new(r"don't\(\).+?do\(\)").unwrap();
-    re.replace_all(input, "").into_owned()
+fn eval(program: Vec<Op>, with_disabled_memory: bool) -> i32 {
+    let mut disabled = false;
+    let mut sum = 0;
+    for op in program {
+        match op {
+            Op::Mul(a, b) => {
+                if !with_disabled_memory || !disabled {
+                    sum += a*b;
+                }
+            }
+            Op::Do => disabled = false,
+            Op::DoNot => disabled = true,
+            Op::Unknown => {},
+        }
+    }
+
+    sum
 }
 
 pub fn process_part1(input: &str) -> String {
-    let mem = process_input(input);
-    println!("{:?}", mem);
-    mem
-        .iter()
-        .fold(0, |sum, op| sum + (op.p1*op.p2))
-        .to_string()
-
+    let program = parse_input(input);
+    eval(program, false).to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let removed = remove_disabled_memory(input);
-    println!("{}", removed);
-
-    let mem = process_input(removed.as_str());
-    // println!("{:?}", mem);
-    mem
-        .iter()
-        .fold(0, |sum, op| sum + (op.p1*op.p2))
-        .to_string()
+    let program = parse_input(input);
+    eval(program, true).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT1: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     const INPUT2: &str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_process_part1() {
-        println!("{}", process_part1(INPUT));
+        println!("{}", process_part1(INPUT1));
     }
 
     #[test]
